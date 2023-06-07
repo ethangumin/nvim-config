8cad2a2d949aca034312e5b71b938d73109d4536
egumin <ethangumin@gmail.com>
2023-06-06 22:25:49 -0400

    clangd and cmake

--- lua/plugins-setup.lua
+++ lua/plugins-setup.lua
@@ -1,19 +1,19 @@
 -- auto install packer if not installed
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
-    vim.cmd([[packadd packer.nvim]])
-    return true
-  end
-  return false
+    local fn = vim.fn
+    local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+        vim.cmd([[packadd packer.nvim]])
+        return true
+    end
+    return false
 end
 local packer_bootstrap = ensure_packer() -- true if packer was just installed
 
 -- autocommand that reloads neovim and installs/updates/removes plugins
 -- when file is saved
-vim.cmd([[ 
+vim.cmd([[
   augroup packer_user_config
     autocmd!
     autocmd BufWritePost plugins-setup.lua source <afile> | PackerSync
@@ -23,67 +23,69 @@ vim.cmd([[
 -- import packer safely
 local status, packer = pcall(require, "packer")
 if not status then
-  return
+    return
 end
 
 -- add list of plugins to install
 return packer.startup(function(use)
-  -- packer can manage itself
-  use("wbthomason/packer.nvim")
+    -- packer can manage itself
+    use("wbthomason/packer.nvim")
 
-  -- required for telescope
-  use("nvim-lua/plenary.nvim") -- lua functions that many plugins use
+    -- required for telescope
+    use("nvim-lua/plenary.nvim") -- lua functions that many plugins use
 
-  -- treesitter configuration
-  use({
-    "nvim-treesitter/nvim-treesitter",
-    run = function()
-      local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
-      ts_update()
-    end,
-  })
-
-  -- commenting with gc
-  use("numToStr/Comment.nvim")
-
-  -- fuzzy finding w/ telescope
-  use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
-  use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" }) -- fuzzy finder
-
-  use {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v2.x',
-    requires = {
-      -- LSP Support
-      {'neovim/nvim-lspconfig'},             -- Required
-      {                                      -- Optional
-        'williamboman/mason.nvim',
+    -- treesitter configuration
+    use({
+        "nvim-treesitter/nvim-treesitter",
         run = function()
-          pcall(vim.cmd, 'MasonUpdate')
+            local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+            ts_update()
         end,
-      },
-      {'williamboman/mason-lspconfig.nvim'}, -- Optional
+    })
 
-      -- Autocompletion
-      {'hrsh7th/nvim-cmp'},     -- Required
-      {'hrsh7th/cmp-nvim-lsp'}, -- Required
-      {'L3MON4D3/LuaSnip'},     -- Required
+    -- commenting with gc
+    use("numToStr/Comment.nvim")
+
+    -- fuzzy finding w/ telescope
+    use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
+    use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" })        -- fuzzy finder
+
+    -- lsp
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' }, -- Required
+            {
+                -- Optional
+                'williamboman/mason.nvim',
+                run = function()
+                    pcall(vim.cmd, 'MasonUpdate')
+                end,
+            },
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },     -- Required
+            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+            { 'L3MON4D3/LuaSnip' },     -- Required
+        }
     }
-  }
 
-  -- auto closing
-  use("windwp/nvim-autopairs") -- autoclose parens, brackets, quotes, etc...
-  use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" }) -- autoclose tags
+    -- auto closing
+    use("windwp/nvim-autopairs")                                 -- autoclose parens, brackets, quotes, etc...
+    use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" }) -- autoclose tags
 
-  -- tmux & split window navigation
-  use("christoomey/vim-tmux-navigator")
-  use("szw/vim-maximizer") -- maximizes and restores current window
+    -- tmux & split window navigation
+    use("christoomey/vim-tmux-navigator")
+    use("szw/vim-maximizer") -- maximizes and restores current window
 
-  -- colors
-  use("bluz71/vim-moonfly-colors") -- color scheme
+    -- colors
+    use("bluz71/vim-moonfly-colors")
 
-  -- This must be at the end after all plugins
-  if packer_bootstrap then
-    require("packer").sync()
-  end
+    -- This must be at the end after all plugins
+    if packer_bootstrap then
+        require("packer").sync()
+    end
 end)
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,11 +1,11 @@
 local lspconfig_status, lsp = pcall(require, "lsp-zero")
 if not lspconfig_status then
-  return
+    return
 end
 
 local cmp_status, cmp = pcall(require, "cmp")
 if not cmp_status then
-  return
+    return
 end
 
 lsp.preset("recommended")
@@ -16,18 +16,20 @@ lsp.nvim_workspace()
 lsp.ensure_installed({
     'lua_ls',
     'rust_analyzer',
+    'clangd',
+    'cmake',
 })
 
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
     ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
     ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
     ['<C-Space>'] = cmp.mapping.complete(), -- start completion
 })
 
 lsp.set_preferences({
-    sign_icons = { }
+    sign_icons = {}
 })
 
 lsp.setup_nvim_cmp({
@@ -36,18 +38,18 @@ lsp.setup_nvim_cmp({
 
 local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
 local lsp_format_on_save = function(bufnr)
-  vim.api.nvim_clear_autocmds({group = augroup, buffer = bufnr})
-  vim.api.nvim_create_autocmd('BufWritePre', {
-    group = augroup,
-    buffer = bufnr,
-    callback = function()
-      vim.lsp.buf.format()
-    end,
-  })
+    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+    vim.api.nvim_create_autocmd('BufWritePre', {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+            vim.lsp.buf.format()
+        end,
+    })
 end
 
 lsp.on_attach(function(client, bufnr)
-    local opts = {buffer = bufnr, remap = false}
+    local opts = { buffer = bufnr, remap = false }
 
     lsp_format_on_save(bufnr)
 
